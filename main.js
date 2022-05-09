61bfdea2b851c319ced0871c20e0c5d668fb7095
naerix <alexxstrings@gmail.com>
2022-05-09 19:43:15 -0300

    subida de inicio de sesion

---
+++ main.js
@@ -0,0 +1,17 @@
+
+const user=prompt("registre un nombre de usuario");
+const pass = prompt("registre una contraseña"); 
+alert("registro exitoso, por favor pulse -ACEPTAR- para iniciar sesion")
+function login(){
+    const usuario = document.getElementById("usuario").value;
+    const contraseña = document.getElementById("contraseña").value;
+
+    if( usuario === user && contraseña === pass){
+        alert("inicio de sesion exitoso");
+    }
+
+    else{
+        alert("la cagaste");
+    }
+}
+
